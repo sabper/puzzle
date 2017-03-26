1f6f18352306149df35713049c92fd9b0904c54d
윤종원 <jwyoon@barogo.com>
2017-03-26 17:07:35 +0900

    소스 정리
    - login js file 분리

--- web/public/js/login.js
+++ web/public/js/login.js
@@ -1,18 +1,26 @@
 $(function() {
-  $('#login_form').submit(function (event) {
-    var data = ($(this).serializeArray())
-    $.ajax({
-      type: 'POST',
-      url: '/api/v1/users/user/login',
-      data: data,
-      success: function (data) {
-        console.log(data)
-        window.location.replace('/')
-      },
-      error: function (data) {
-        alert('로그인 실패')
-      }
-    })
-    event.preventDefault()
-  })
+  // 로그인
+  $("#utloginbt").click(function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    var email = $('#email').val();
+    var password = $('#password').val();
+
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+      // Handle Errors here.
+      // 등록되지 않은 이메일 ... 등등 오류코드 확인 : https://firebase.google.com/docs/reference/js/firebase.auth.Auth#signInWithEmailAndPassword
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode, errorMessage);
+    });
+  });
+
+  // 로그아웃
+  $("#logoutBtn").click(function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    firebase.auth().signOut();
+  });
 })
